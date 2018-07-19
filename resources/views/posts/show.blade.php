3aa59707e86dc19e087ba12e1573c837e1e49d19
Antony Cava <gcavanunez@gmail.com>
2018-07-18 23:51:33 -0500

    done auth and guards

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,11 +8,15 @@
     
     </div>
     <hr>
-    <small>Wrten on {{$single_post->created_at}}</small>
+    <small>Wrten on {{$single_post->created_at}} by  {{$single_post->user->name}}</small>
     <hr>
+    @if(!Auth::guest())
+    @if(Auth::user()->id == $single_post->user_id)
 <a href="/posts/{{$single_post->id}}/edit" class="btn btn-default">Edit</a>
 {!!Form::open(['action'=>['PostsController@destroy', $single_post->id],'method'=>'POST','class'=>'pull-right'])!!}
     {{Form::hidden('_method', 'DELETE')}}
     {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
 {!!Form::close()!!}
+@endif
+@endif
 @endsection
